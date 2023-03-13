11a6d7d77beba3fc6c804b773e6a0cf379185de7
mayurgupta7234-code <mayurgupta7234@gmail.com>
2023-03-13 11:06:56 +0530

    [ADDED] models to template

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 # import login required decorator
 from django.contrib.auth.decorators import login_required
+from .models import MLModel, MLModelInput
 
 # Create your views here.
 @login_required(login_url="/auth/login/")
 def home(request):
-    return render(request, "home/index.html")
+    # get all the models
+    ml_models = MLModel.objects.filter(user=request.user)
+    context = {
+        "ml_models": ml_models
+    }
+    return render(request, "home/index.html", {"data": context})
