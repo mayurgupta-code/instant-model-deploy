b4ff91a1da51a7da8455e4f9aaa0b24a9c69e0c4
bhavesh <bhaveshvaishnav555@gmail.com>
2023-03-14 11:37:30 +0530

    merged

--- home/models.py
+++ home/models.py
@@ -9,10 +9,14 @@ from django.db import models
 # updated_at: the time when the model is updated
 # accuracy: the accuracy of the model
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
 class MLModel(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, null=True, blank=True)
-    # model_file = models.FileField(upload_to='models/')
+    description = models.CharField(max_length=1000, default="", null=True, blank=True)
+    model_file = models.FileField(upload_to=user_directory_path, null=True, blank=True)
     accuracy = models.FloatField(default=0.0)
     user = models.ForeignKey('auth.User', on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -34,17 +38,17 @@ class MLModel(models.Model):
 
 class MLModelInput(models.Model):
     INPUT_TYPE_CHOICES = (
-        ('text', 'Text'),
-        ('int', 'Integer'),
-        ('float', 'Float'),
-        ('file', 'File'),
-        ('image', 'Image'),
+        ('text', 'str'),
+        ('checkbox', 'checkbox'),
+        ('number', 'float'),
+        ('file', 'file'),
+        ('image', 'image'),
     )
     name = models.CharField(max_length=100)
-    attribute = models.CharField(max_length=100, default='')
+    attribute = models.CharField(max_length=100, default='', null=True, blank=True)
     description = models.CharField(max_length=1000, null=True, blank=True)
-    input_type = models.CharField(max_length=100)
-    model = models.ForeignKey(MLModel, on_delete=models.CASCADE)
+    input_type = models.CharField(max_length=100, choices=INPUT_TYPE_CHOICES, default='float')
+    model = models.ForeignKey(MLModel, on_delete=models.CASCADE, related_name='inputs')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
